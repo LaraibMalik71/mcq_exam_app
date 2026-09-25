d1447a4d01b67ae42134eae5335c4bc7c6adfc92
LaraibMalik71 <hh2999720@gmail.com>
2026-09-25 10:48:09 -0700

    Clean up main.dart import

--- home_screen.dart
+++ home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import '../main.dart';
+import 'main.dart';
 import 'quiz_screen.dart';
 
 class HomeScreen extends StatefulWidget {
